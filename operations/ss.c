9dc914b139bea33172df27b13f1e5c4a94ef2317
Tommaso Piana Agostinetti <topiana-@c1r3p9.42firenze.it>
2025-01-13 16:11:33 +0100

    ss fix

--- operations/ss.c
+++ operations/ss.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ss.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: totommi <totommi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: topiana- <topiana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 17:29:25 by topiana-          #+#    #+#             */
-/*   Updated: 2025/01/12 09:48:28 by totommi          ###   ########.fr       */
+/*   Updated: 2025/01/13 16:10:47 by topiana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,25 @@
 RETURNS: 1 all good, 0 error*/
 int	ss(t_stack **head_a, t_stack **head_b)
 {
-	if (!sa(head_a, head_b))
+	t_stack	*third_a;
+	t_stack	*third_b;
+	t_stack	*second_a;
+	t_stack	*second_b;
+
+	if (!*head_a || !(*head_a)->next)
 		return (0);
-	if (!sb(head_a, head_b))
+	if (!*head_b || !(*head_b)->next)
 		return (0);
+	third_a = (*head_a)->next->next;
+	third_b = (*head_b)->next->next;
+	second_a = (*head_a)->next;
+	second_b = (*head_b)->next;
+	(*head_a)->next = third_a;
+	(*head_b)->next = third_b;
+	second_a->next = *head_a;
+	second_b->next = *head_b;
+	*head_a = second_a;
+	*head_b = second_b;
 	ft_printf("ss\n");
 	return (1);
 }
